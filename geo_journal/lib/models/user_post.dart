e8e1d7d496b60cb16ad232cff649ad56ff3cee03
brushandspatula <brushandspatula@gmail.com>
2025-12-27 19:51:06 +0100

    Added UserPost model

---
+++ geo_journal/lib/models/user_post.dart
@@ -0,0 +1,18 @@
+class UserPost {
+  final int id;
+  final String title;
+  final String description;
+  final double latitude;
+  final double longitude;
+
+
+UserPost({
+  required this.id,
+  required this.title,
+  required this.description,
+  required this.latitude,
+  required this.longitude,
+});
+
+
+}
